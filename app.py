9a666450040757f575e47dfcb89878569445d0ee
Mateus Marmitt <lmateus.marmitt@gmail.com>
2025-04-04 17:40:45 +1100

    Initial deploy to Render

--- app.py
+++ app.py
@@ -199,4 +199,5 @@ def generate_recommendations(sections, missing_keywords):
     return tips
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
